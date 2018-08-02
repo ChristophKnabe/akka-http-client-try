5664fb0c6808110fb1446896b057813b6990b97e
Christoph Knabe <knabe@beuth-hochschule.de>
2018-08-02 18:03:33 +0200

    Smooth Shutdown. Adjust trust store password. Let the HTTP GET future timeout instead of the actor. Let after shutting down all connection pools pass 10 seconds before shutting down the actor system.

--- src/main/scala/webclient/WebClientApp.scala
+++ src/main/scala/webclient/WebClientApp.scala
@@ -11,57 +11,70 @@ package webclient
 object WebClientApp extends App {
 
   import akka.actor.{ ActorSystem, Props }
-  import scala.concurrent.{ Await }
-  import scala.concurrent.duration.Duration
+  import java.net.{URL, URLConnection}
+  import javax.net.ssl.SSLException
 
-  implicit val system = ActorSystem("client-system")
+  val uriArg = "https://www.berlin.de/" //"http://akka.io"
+
+  _adjustTrustStorePassword(uriArg)
+
+  implicit val system: ActorSystem = ActorSystem("client-system")
   val clientActor = system.actorOf(Props[ClientActor], "client-actor")
+
+  /**Set the Oracle default trust store password, if https access does not work. See https://github.com/ChristophKnabe/tool-instruction/blob/master/maven-trustAnchors-problem.md*/
+  private def _adjustTrustStorePassword(uriArg: String): Unit = {
+    val url = new URL(uriArg)
+    if(url.getProtocol.equals("http")){ //unsecure
+      return; //Does not need trustStore access.
+    }
+    val con: URLConnection = url.openConnection
+    try{
+      con.connect()
+    }catch{
+      case ssle: SSLException if ssle.getMessage == "java.lang.RuntimeException: Unexpected error: java.security.InvalidAlgorithmParameterException: the trustAnchors parameter must be non-empty" =>
+        System.setProperty("javax.net.ssl.trustStorePassword", "changeit")
+      //Now https access should work!
+    }
+  }
 
 }
 
-import java.util.concurrent.TimeUnit
 
-import akka.actor.{ Actor, ActorLogging, PoisonPill, ReceiveTimeout }
+import akka.actor.{Actor, ActorLogging, PoisonPill}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.stream.{ ActorMaterializer, ActorMaterializerSettings }
-import akka.stream.scaladsl.{ Framing }
-import akka.util.ByteString
-import akka.util.Timeout
+import akka.http.scaladsl.model.{Uri, _}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 
-import scala.util.{ Failure, Success }
+import scala.util.{Failure, Success}
 
 class ClientActor extends Actor with ActorLogging {
-
-  val uri: Uri = "https://www.berlin.de" //"http://akka.io"
 
   import akka.pattern.pipe
   import context.dispatcher
   import scala.concurrent.duration._
+
+  private val uri: Uri = WebClientApp.uriArg
 
   private final implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
 
   private val http = Http(context.system)
   private val startTime = System.currentTimeMillis
 
-  override def preStart() = {
+  override def preStart(): Unit = {
     http.singleRequest(HttpRequest(uri = uri)).pipeTo(self)
-    context.setReceiveTimeout(Duration(20, TimeUnit.SECONDS))
     println(s"GET request $uri started.")
   }
 
-  def receive = {
+  def receive: Actor.Receive = {
     case HttpResponse(StatusCodes.OK, headers, entity, _) =>
       log.info("Getting page {} ...", uri)
       entity.dataBytes.
-        //with framing according to http://doc.akka.io/docs/akka-http/10.0.9/scala/http/implications-of-streaming-http-entity.html#consuming-the-http-response-entity-client-
-        via(Framing.delimiter(ByteString("href=\""), maximumFrameLength = 65000, allowTruncation = true)).
         runForeach {
-          frame =>
-            println("====================================Got response frame====================================\n" + frame.utf8String)
+          chunk =>
+            println("====================================Got response chunk====================================\n" + chunk.utf8String)
         }.onComplete {
           case Success(done) =>
-            println(s"GET request $uri completed within $elapsedMillis millis.")
+            log.info("GET request {} completed within {} millis.", uri, elapsedMillis)
             _terminate()
           case Failure(t) =>
             log.error("GET request {} dataBytes read completed with Failure {} within {} millis.", uri, t, elapsedMillis)
@@ -70,10 +83,6 @@ class ClientActor extends Actor with ActorLogging {
     case resp @ HttpResponse(code, _, _, _) =>
       log.info("Request failed, response code: " + code)
       resp.discardEntityBytes()
-      _terminate()
-
-    case ReceiveTimeout =>
-      log.error("GET request {} was not answered within {} millis.", uri, elapsedMillis)
       _terminate()
 
     case unexpected =>
@@ -86,13 +95,14 @@ class ClientActor extends Actor with ActorLogging {
   private def _terminate(): Unit = {
     val system = context.system
     import system.dispatcher
-    //Give the actor some time to terminate before terminating the actor system:
-    system.scheduler.scheduleOnce(2.seconds) {
-      log.info("Going to shut down the system...")
-      http.shutdownAllConnectionPools()
+    log.info("Shut down all HTTP connection pools...")
+    for {
+      _ <- http.shutdownAllConnectionPools()
+      _ = self ! PoisonPill
+    } yield system.scheduler.scheduleOnce(20.seconds) { //Give the connections double time of their idle timeout to be closed before terminating the actor system:
+      log.info("Shut down the ActorSystem...")
       system.terminate()
     }
-    self ! PoisonPill
   }
 
 }
